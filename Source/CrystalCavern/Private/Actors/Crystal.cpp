2fdd23c3973a8e36a9a8931ace315d0dcc47606e
Karim_Gamal0 <karim.gamal.cs@gmail.com>
2022-04-03 11:17:27 +0200

    Add Timeline to Crystals and Refactor code

--- Source/CrystalCavern/Private/Actors/Crystal.cpp
+++ Source/CrystalCavern/Private/Actors/Crystal.cpp
@@ -2,6 +2,10 @@
 
 
 #include "Actors/Crystal.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h" 
+#include "Components/TimelineComponent.h"
+#include "Components/PointLightComponent.h"
 
 // Sets default values
 ACrystal::ACrystal()
@@ -9,8 +13,8 @@ ACrystal::ACrystal()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Component"));
-	RootComponent = SceneComponent;
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	RootComponent = CollisionComponent;
 
 	CrystalTopHalf = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Crystal Top Half"));
 	CrystalTopHalf->SetupAttachment(RootComponent);
@@ -18,6 +22,11 @@ ACrystal::ACrystal()
 	CrystalBottomHalf = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Crystal Bottom Half"));
 	CrystalBottomHalf->SetupAttachment(CrystalTopHalf);
 
+	APointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("Point Light"));
+	APointLight->SetupAttachment(RootComponent);
+
+	//Add Timeline Component
+	CrystalTimelineComp = CreateDefaultSubobject<UTimelineComponent>(TEXT("CrystalTimelineComp"));
 }
 
 // Called when the game starts or when spawned
@@ -25,6 +34,34 @@ void ACrystal::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//Intialize varialbles
+	StartLerpLocation = GetActorLocation();
+	StartScale = GetActorScale3D();
+
+	PointLightStartIntensity = APointLight->Intensity;
+
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ACrystal::OnOverlapBegin);
+	CrystalTopHalf->OnComponentBeginOverlap.AddDynamic(this, &ACrystal::OnMeshOverlapBegin);
+
+	FOnTimelineFloat OnTimelineLocationCallback;
+	FOnTimelineFloat OnTimelineScaleCallback;
+	FOnTimelineFloat OnTimelineLightICallback;
+
+	FOnTimelineEventStatic OnTimeLineFinishedCallback;
+
+	OnTimelineLocationCallback.BindUFunction(this, TEXT("UpdateTimelineLocationComp"));
+	OnTimelineScaleCallback.BindUFunction(this, TEXT("UpdateTimelineScaleComp"));
+	OnTimelineLightICallback.BindUFunction(this, TEXT("UpdateTimelineLightComp"));
+
+		OnTimeLineFinishedCallback.BindUFunction(this, TEXT("FinishedTimeLineComp"));
+
+	if (CrystalTimelineFloatLocationCurve && CrystalTimelineFloatScaleCurve && CrystalTimelineFloatLightIntensityCurve) {
+		CrystalTimelineComp->AddInterpFloat(CrystalTimelineFloatLocationCurve, OnTimelineLocationCallback);
+		CrystalTimelineComp->AddInterpFloat(CrystalTimelineFloatScaleCurve, OnTimelineScaleCallback);
+		CrystalTimelineComp->AddInterpFloat(CrystalTimelineFloatLightIntensityCurve, OnTimelineLightICallback);
+
+		CrystalTimelineComp->SetTimelineFinishedFunc(OnTimeLineFinishedCallback);
+	}
 }
 
 // Called every frame
@@ -32,5 +69,69 @@ void ACrystal::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (CrystalTimelineComp) {
+		//CrystalTimelineComp->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, NULL);
+	}
 }
 
+void ACrystal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && OtherActor != this) {
+
+		if (OtherActor == UGameplayStatics::GetPlayerPawn(GetWorld(), 0)) {
+			CrystalTimelineComp->Play();
+		}
+	}
+}
+
+void ACrystal::OnMeshOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && OtherActor != this) {
+		OtherActor = Cast<AActor>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+
+		if (OtherActor) {
+			Destroy();
+		}
+	}
+}
+
+
+void ACrystal::UpdateTimelineLocationComp(float output)
+{
+	MoveToPlayer(output);
+}
+
+void ACrystal::UpdateTimelineScaleComp(float output) {
+	ScaleCrystal(output);
+}
+
+void ACrystal::UpdateTimelineLightComp(float output) {
+	LightIntensityCrystal(output);
+}
+
+void ACrystal::FinishedTimeLineComp()
+{
+	Destroy();
+}
+
+void ACrystal::MoveToPlayer(float output)
+{
+	Endlocation = FMath::Lerp(StartLerpLocation, UGameplayStatics::GetPlayerPawn(GetWorld(), 0)->GetActorLocation(), output);
+	SetActorLocation(Endlocation);
+}
+
+void ACrystal::ScaleCrystal(float output)
+{
+	EndScale = FMath::Lerp(StartScale, FVector(0.0, 0.0, 0.0), output);
+	SetActorScale3D(EndScale);
+}
+
+void ACrystal::LightIntensityCrystal(float output) {
+	PointLightEndIntensity = FMath::Lerp(PointLightStartIntensity, 0.0f, output);
+
+	if (APointLight)
+	{
+		UE_LOG(LogTemp, Error, TEXT("light"));
+		APointLight->SetIntensity(PointLightEndIntensity);
+	}
+}
--- Source/CrystalCavern/Public/Actors/Crystal.h
+++ Source/CrystalCavern/Public/Actors/Crystal.h
@@ -6,29 +6,85 @@
 #include "GameFramework/Actor.h"
 #include "Crystal.generated.h"
 
+class USphereComponent;
+class UTimelineComponent;
+class UCurveFloat;
+class UPointLightComponent;
 UCLASS()
 class CRYSTALCAVERN_API ACrystal : public AActor
 {
 	GENERATED_BODY()
 
 public:
+	//Functions
 	// Sets default values for this actor's properties
 	ACrystal();
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+		void UpdateTimelineLocationComp(float output);
+
+	UFUNCTION()
+		void UpdateTimelineScaleComp(float output);
+
+	UFUNCTION()
+		void UpdateTimelineLightComp(float output);
+
+
+	UFUNCTION()
+	    void FinishedTimeLineComp();
+
 protected:
+	UTimelineComponent* CrystalTimelineComp;
+	//FOnTimelineFloat UpdateFunctionFloat;
+
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+
 private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
-		USceneComponent* SceneComponent;
+
+	//Variables
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* CrystalTimelineFloatLocationCurve;
+
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* CrystalTimelineFloatScaleCurve;
+
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* CrystalTimelineFloatLightIntensityCurve;
+
+	FVector StartLerpLocation;
+	FVector StartScale;
+	FVector Endlocation;
+	FVector EndScale;
+	float PointLightStartIntensity = 0.0f;
+	float PointLightEndIntensity;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 		UStaticMeshComponent* CrystalTopHalf;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
 		UStaticMeshComponent* CrystalBottomHalf;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+		UPointLightComponent* APointLight;
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnMeshOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	void MoveToPlayer(float output);
+	void ScaleCrystal(float output);
+	void LightIntensityCrystal(float output);
+
 };
